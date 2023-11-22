8fa6641872c3690d8c8ac36deb0c5a7f28330209
bear <962338171@qq.com>
2023-11-22 09:38:04 +0800

    初始化redis

--- src/main/java/com/bear/rbac/common/RedisService.java
+++ src/main/java/com/bear/rbac/common/RedisService.java
@@ -2,34 +2,37 @@ package com.bear.rbac.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 @Component
+@SuppressWarnings(value = {"unchecked", "rawtypes"})
 public class RedisService<T> {
 
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    private RedisTemplate redisTemplate;
 
 
     public void set(String key, T value) {
 
-        redisTemplate.opsForValue().set(key,value);
+        redisTemplate.opsForValue().set(key, value);
     }
 
     public void set(String key, T value, long timeout) {
-        redisTemplate.opsForValue().set(key,value,timeout, TimeUnit.SECONDS);
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.SECONDS);
     }
 
     public void set(String key, T value, long timeout, TimeUnit timeUnit) {
-        redisTemplate.opsForValue().set(key,value,timeout,timeUnit);
+        redisTemplate.opsForValue().set(key, value, timeout, timeUnit);
     }
 
     public T get(String key) {
 
-        return (T) redisTemplate.opsForValue().get(key);
+        ValueOperations<String, T> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
     }
 
     public Boolean expire(final String key, final long timeout) {
@@ -38,8 +41,7 @@ public class RedisService<T> {
     }
 
 
-    public Boolean expire(final String key, final long timeout, final TimeUnit unit)
-    {
+    public Boolean expire(final String key, final long timeout, final TimeUnit unit) {
         return redisTemplate.expire(key, timeout, unit);
     }
 
@@ -47,14 +49,7 @@ public class RedisService<T> {
         return redisTemplate.delete(key);
     }
 
-    /**
-     * 获得缓存的基本对象列表
-     *
-     * @param pattern 字符串前缀
-     * @return 对象列表
-     */
-    public Collection<String> keys(String pattern)
-    {
+    public Collection<String> keys(String pattern) {
         return redisTemplate.keys(pattern);
     }
 
@@ -63,7 +58,7 @@ public class RedisService<T> {
         return redisTemplate.hasKey(key);
     }
 
-    public long incr(String key) {
+    public Long incr(String key) {
 
         return redisTemplate.opsForValue().increment(key);
     }
